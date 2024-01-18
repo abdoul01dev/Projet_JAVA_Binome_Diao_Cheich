325ed47903e005566141642bccfff480f156cd06
Cheich Yalaweogo <cheich.yalaweogo61@gmail.com>
2024-01-18 17:20:49 +0000

    Utilisateurs, tableau de bord etc

---
+++ Transport/src/application/GroupeController.java
@@ -0,0 +1,50 @@
+package application;
+
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class GroupeController {
+
+    @FXML
+    private TextField DescriptionGroupe;
+
+    @FXML
+    private Label IdLabel;
+
+    @FXML
+    private TextField NomGroupe;
+
+    @FXML
+    private BorderPane borderpane;
+
+    @FXML
+    private JFXButton btnAnnulerG;
+
+    @FXML
+    private JFXButton btnVF;
+
+    @FXML
+    private JFXButton btnValid;
+
+    @FXML
+    void AnnulerG(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ValidF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Valide(ActionEvent event) {
+
+    }
+
+}
+
